35abd26ba565323f5c60432f91e0d75b1ac62a94
Ewan Mellor <ewan@tipbit.com>
2015-05-02 20:04:24 -0700

    Added.

--- README.md
+++ README.md
@@ -3,6 +3,43 @@ git-diff-image
 
 This is an extension to 'git diff' that provides support for diffing images.
 
+Platforms
+---------
+
+Only OS X at the moment.  Patches welcome!
+
+Examples
+--------
+
+```
+$ git diff
+--- a/anImageThatHasChanged.jpg
++++ b/anImageThatHasChanged.jpg
+@@ -1,5 +1,5 @@
+ ExifTool Version Number         : 9.76
+-File Size                       : 133 kB
++File Size                       : 54 kB
+ File Access Date/Time           : 2015:05:02 20:01:21-07:00
+ File Type                       : JPEG
+ MIME Type                       : image/jpeg
+
+$ git diff-image
+# The same output as above, *and* a montage of the visual differences will be
+# generated and opened in Preview.
+```
+
+Installation
+------------
+
+Install exiftool and ImageMagick.  (The script will cope with these missing,
+but it's not going to be very exciting without them.)
+
+```
+brew install exiftool imagemagick
+```
+
+Run the install script, which will configure your global git config for you.
+
 Public domain dedication
 ------------------------
 
---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cols=$(tput cols)
+
+
+readlink_f() {
+  if [ $(uname) = 'Darwin' ]
+  then
+    local d=$(echo "${1%/*}")
+    local f=$(basename "$1")
+    (cd "$d" && echo "$(pwd -P)/$f")
+  else
+    readlink -f "$1"
+  fi
+}
+
+
+addattr() {
+  ext="$1"
+
+  if ! grep -qE "^\*.$ext diff=image\$" "$attributesfile"
+  then
+    if grep -qE "^\*.$ext" "$attributesfile"
+    then
+      fold -s -w$cols >&2 <<EOS
+$attributesfile already has *.$ext configured, but not the way that this script requires.
+
+If you want to use git-image-diff with $ext files, you must add a diff=image attribute yourself.
+
+
+EOS
+    else
+      echo "+ echo '*.$ext diff=image' >>'$attributesfile'"
+      echo "*.$ext diff=image" >>"$attributesfile"
+    fi
+  fi
+}
+
+
+thisdir=$(dirname $(readlink_f "$0"))
+thisdir_tilde="${thisdir/#$HOME/~}"
+
+attributesfile_tilde=$(git config --global core.attributesfile || true)
+attributesfile="${attributesfile_tilde/#\~/$HOME}"
+if [ -z "$attributesfile" ]
+then
+  attributesfile="$HOME/.gitattributes"
+  attributesfile_tilde="~/.gitattributes"
+  echo "+ git config --global core.attributesfile '$attributesfile_tilde'"
+  git config --global core.attributesfile "$attributesfile_tilde"
+fi
+
+if [ ! -f "$attributesfile" ]
+then
+  if [ ! -e "$attributesfile" ]
+  then
+    echo "+ touch '$attributesfile'"
+    touch "$attributesfile"
+  else
+    echo "$attributesfile is not a regular file!  I give up." >&2
+    exit 1
+  fi
+fi
+
+addattr gif
+addattr jpeg
+addattr jpg
+addattr png
+
+echo '+ git config --global alias.diff-image '"'"'!f() { GIT_DIFF_IMAGE_ENABLED=1 git diff "$@"; }; f'"'"
+git config --global alias.diff-image '!f() { GIT_DIFF_IMAGE_ENABLED=1 git diff "$@"; }; f'
+
+echo "+ git config --global diff.image.command '$thisdir_tilde/git_diff_image'"
+git config --global diff.image.command "$thisdir_tilde/git_diff_image"
